1b65a32c4f5d484f8ef70f511cf3c3eebdbe6048
Eric Vere <erkver250@gmail.com>
2018-12-27 00:20:31 -0600

    Updated landing page

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import _ from 'lodash';
 import printMe from './print.js';
 import "./test.scss";
 
-function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+const component = () => {
+  const mainDiv = document.createElement('div');
+  mainDiv.className = "main-div";
+  const element = document.createElement('div');
+  element.className = "inner-div";
+  const btn = document.createElement('button');
+  btn.className = "auth-btn"
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Off the Record'], ' ');
 
-  btn.innerHTML = 'Click me and check the console!';
+  btn.innerHTML = 'Sign Up!';
   btn.onclick = printMe;  // onclick event is bind to the original printMe function
 
   element.appendChild(btn);
@@ -20,7 +24,7 @@ let element = component(); // Store the element to re-render on print.js changes
 document.body.appendChild(element);
 
 if (module.hot) {
-  module.hot.accept('./print.js', function () {
+  module.hot.accept('./print.js', () => {
     console.log('Accepting the updated printMe module!');
     document.body.removeChild(element);
     element = component(); // Re-render the "component" to update the click handler
